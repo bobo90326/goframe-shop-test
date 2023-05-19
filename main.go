0c2ba7b860e23390948982d35b53c93722698aa2
mac <bobo90326>
2023-05-19 17:40:02 +0800

    test

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	_ "github.com/gogf/gf/contrib/drivers/mysql/v2"
-	"github.com/gogf/gf/v2/os/gctx"
 	"goframe-shop-test/internal/cmd"
 	_ "goframe-shop-test/internal/logic"
 	_ "goframe-shop-test/internal/packed"
+
+	_ "github.com/gogf/gf/contrib/drivers/mysql/v2"
+	"github.com/gogf/gf/v2/os/gctx"
 )
 
-//启动
+// 启动
 func main() {
 	cmd.Main.Run(gctx.New())
 }
